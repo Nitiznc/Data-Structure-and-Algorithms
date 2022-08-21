a0a8df9e205455263e2c6b1367b68a91d2e30a9b
Nitish Chaudhary <78808175+Nitiznc@users.noreply.github.com>
2022-08-21 19:10:07 +0530

    Create ceiling-number.java

---
+++ ceiling-number.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+public class MyClass {
+    public static void main(String args[]) {
+        int[] arr = {2,3,9,14,16,18,20};
+        int target = 15;
+        int ans = ceiling(arr, target);
+        System.out.println(ans);
+    }
+    static int ceiling(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length-1;
+        while(end>=start){
+            int mid = start + (end-start)/2;
+            if (arr[mid]>target){
+                end=mid-1;
+            }
+            else if (arr[mid]<target){
+                start = mid+1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return start;
+    }
+}
