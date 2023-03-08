54b396ded12d42c4a4bfc714a2c4499e1c09a4d0
Nitish Chaudhary <78808175+Nitiznc@users.noreply.github.com>
2023-03-08 21:48:08 +0530

    Update First-and-Last-Position-in-Sorted-Array.java

--- First-and-Last-Position-in-Sorted-Array.java
+++ First-and-Last-Position-in-Sorted-Array.java
@@ -1,30 +1,34 @@
-class Solution {
-    public int[] searchRange(int[] arr, int target){
-        int[] ans = {-1,-1};
-        int start =  search(arr, target, true);
-        int end =  search(arr, target, false);
-        ans[0]=start;
-        ans[1]=end;
-        return ans;
+import java.util.*;
+
+public class MyClass{
+    public static void main(String[] args){
+        int[] arr = {1,1,1,2,2,2,3,3,3,3,3,3,3,3,3,3,3,3,3,3};
+        int[] ans = {-1, -1};
+        int start = search(arr, true, 1);
+        int end = search(arr, false, 1);
+        ans[0] = start;
+        ans[1] = end;
+        System.out.println(Arrays.toString(ans));
     }
-    int search(int[] arr, int target, boolean firstIndex){
-        int ans= -1;
+    
+    private static int search(int[] arr, boolean index, int target){
+        int ans = -1;
         int start = 0;
         int end = arr.length-1;
         while(start<=end){
-            int mid = start+(end-start)/2;
+            int mid = start + (end-start)/2;
             if(arr[mid]>target){
-                end=mid-1;
+                end = mid-1;
             }
-            else if (arr[mid]<target){
-                start=mid+1;
+            else if(arr[mid]<target){
+                start = mid+1;
             }
             else{
                 ans = mid;
-                if (firstIndex){
+                if(index){
                     end = mid-1;
                 }
-                else {
+                else{
                     start = mid+1;
                 }
             }
